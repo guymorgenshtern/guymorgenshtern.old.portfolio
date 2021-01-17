9588e5356a32c817cd2855eaf2f824dd9460fb0a
guymorgenshtern <33492457+guymorgenshtern@users.noreply.github.com>
2021-01-17 16:01:16 -0500

    Stylistic Changes

--- js/main.js
+++ js/main.js
@@ -10,6 +10,29 @@ $(document).ready(function() {
     }
   });
 
+const chartWrapper = document.querySelector(".chart-wrapper");
+const scrollDown = document.querySelector(".scroll-down");
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+window.addEventListener("scroll", scrollHandler);
+
+function scrollHandler() {
+  window.pageYOffset > 0
+    ? scrollDown.classList.add("is-hidden")
+    : scrollDown.classList.remove("is-hidden");
+  if (isElementInViewport(chartWrapper)) chartWrapper.classList.add("in-view");
+}
+
   /*Smooth scroll between elements */
 
   $(document).on("scroll", onScroll);
